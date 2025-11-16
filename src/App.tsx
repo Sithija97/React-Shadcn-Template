8f988a06bc36763726db60836a2e52af528443a2
Sithija Shehara <nsithijashehara@gmail.com>
2025-11-16 12:25:44 +0530

    shadcn installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Button variant="outline">Button</Button>
     </div>
   );
 };

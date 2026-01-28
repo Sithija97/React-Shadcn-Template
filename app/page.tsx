7cf08b60fe146b1f2e6efdf0a20355fa4feaa017
Sithija Shehara <nsithijashehara@gmail.com>
2026-01-28 21:58:12 +0530

    initial setup & header component

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Linkedin clone</h1>
+    </main>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "links.papareact.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      /* {
+        protocol: "https",
+        hostname: "linkedinclone.blob.core.windows.net",
+      },*/
+    ],
+  } /* config options here */,
+};
+
+export default nextConfig;

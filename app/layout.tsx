f4032604e3c3fea71e752f3b8047effe1af71bf2
Sithija Shehara <nsithijashehara@gmail.com>
2026-02-12 23:47:14 +0530

    title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/common/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
@@ -15,13 +16,18 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "LinkedIn Clone",
-  description: "A LinkedIn clone built with Next.js and Clerk",
-  icons: {
-    icon: "https://static.licdn.com/aero-v1/sc/h/akt4ae504epesldzj74dzred8",
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const user = await currentUser();
+  const displayName = user ? user?.firstName + " " + user?.lastName : "";
+
+  return {
+    title: user ? `LinkedIn | ${displayName}` : "LinkedIn",
+    description: "A LinkedIn clone built with Next.js and Clerk",
+    icons: {
+      icon: "https://static.licdn.com/aero-v1/sc/h/akt4ae504epesldzj74dzred8",
+    },
+  };
+}
 
 export default function RootLayout({
   children,
